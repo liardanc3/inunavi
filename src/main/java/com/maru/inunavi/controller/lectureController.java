f9c8c3eaa203d5f8a235e185fcfcb36a03a62710
Tensor <shtmdqja43@gmail.com>
2021-11-10 16:11:22 +0900

    sex

--- src/main/java/com/maru/inunavi/controller/lectureController.java
+++ src/main/java/com/maru/inunavi/controller/lectureController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 public class lectureController{
-
+    //sex
     private final lectureService _lectureService;
 
     public lectureController(lectureService _lectureService){
