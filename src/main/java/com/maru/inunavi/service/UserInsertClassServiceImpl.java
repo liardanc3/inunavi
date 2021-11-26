8f702d6a4e0c2612974c9b846f502b6af0fb1cfc
Tenser <shtmdqja43@gmail.com>
2021-11-26 17:06:12 +0900

    수정 2

--- src/main/java/com/maru/inunavi/service/UserInsertClassServiceImpl.java
+++ src/main/java/com/maru/inunavi/service/UserInsertClassServiceImpl.java
@@ -13,9 +13,8 @@ public class UserInsertClassServiceImpl implements Service{
         Map<String, String> json = new HashMap<>();
         String id = request.getParameter("id");
         json.put("id", id);
-        if(uDao.insertClass(id, Integer.parseInt(request.getParameter("class_id"))))
-            json.put("success", "true");
-        else json.put("success", "false");
+        uDao.insertClass(id, request.getParameter("class_id"));
+        json.put("success", "true");
         return json;
     }
 
