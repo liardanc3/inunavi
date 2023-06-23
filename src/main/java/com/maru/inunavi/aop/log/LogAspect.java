b53ca8fda01c893cf03f85503550a60ed49ac4a8
liardanc3 <liardanc3@gmail.com>
2023-06-24 08:34:53 +0900

    Fix log logic

--- src/main/java/com/maru/inunavi/aop/log/LogAspect.java
+++ src/main/java/com/maru/inunavi/aop/log/LogAspect.java
@@ -2,6 +2,8 @@ package com.maru.inunavi.aop.log;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -13,12 +15,17 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 @Slf4j
 public class LogAspect {
 
-    @Before("@annotation(com.maru.inunavi.aop.log.Log)")
-    public void logBefore(JoinPoint joinPoint){
-        String ipAddress = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
-                .getRequest()
-                .getRemoteAddr();
+    @Before("@annotation(logger)")
+    public void logBefore(JoinPoint joinPoint, Log logger){
+        if(logger.value().equals("")){
+            String ip = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
+                    .getRequest()
+                    .getRemoteAddr();
 
-        log.info("["+ipAddress+"] calls {}", joinPoint.getSignature().getName());
+            log.info("[" + ip + "] calls {}", joinPoint.getSignature().getName());
+        }
+        else{
+            log.info(logger.value(), joinPoint.getArgs()[0]);
+        }
     }
 }
